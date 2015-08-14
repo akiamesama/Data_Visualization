e8c55c233fe6972db7fdf206c095c0f219d86545
Ahmad Nazhir <anazhir@andrew.cmu.edu>
2015-08-14 12:11:46 -0400

    fix3

--- js/visualization.js
+++ js/visualization.js
@@ -10,11 +10,7 @@
   var post_data1 = {"statements":[{"statement":statement1,"resultDataContents":["graph"]}]}
 
   $.ajax({
-<<<<<<< HEAD
-      type:"POST",headers: {"Authorization": auth},
-=======
       type:"POST",//headers: {"Authorization": auth},
->>>>>>> origin/master
       accept: "application/json",
       contentType:"application/json; charset=utf-8",
       url: "http://52.20.59.19:7474/db/data/transaction/commit",
